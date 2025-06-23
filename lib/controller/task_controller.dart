168fb5a25743fafd51c24dbe31db073b6728f722
Gabriel FV <gabrielvasco906@gmail.com>
2025-06-23 12:44:21 -0300

    feat: add CategoryController and implement TaskController functionality

--- lib/controller/task_controller.dart
+++ lib/controller/task_controller.dart
@@ -1,1 +1,24 @@
+import 'package:flutter/material.dart';
 
+class TaskController extends ChangeNotifier {
+  final List<String> _tasks = [];
+
+  List<String> get tasks => List.unmodifiable(_tasks);
+
+  void addTask(String task) {
+    if (task.isNotEmpty) {
+      _tasks.add(task);
+      notifyListeners();
+    }
+  }
+
+  void removeTask(String task) {
+    _tasks.remove(task);
+    notifyListeners();
+  }
+
+  void clearTasks() {
+    _tasks.clear();
+    notifyListeners();
+  }
+}
