a7120e3631a20a1c08ad949fc03aa96ab4280be4
Gabriel FV <gabrielvasco906@gmail.com>
2025-06-24 19:27:35 -0300

    fix: update NDK version in build.gradle.kts and add devtools_options.yaml for configuration

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -20,60 +20,62 @@ class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 16.0,
-          vertical: 24.0,
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text('Bem vindo!'),
-            SizedBox(height: 12),
-            Text('Pronto para finalizar as tarefas?'),
-            SizedBox(height: 24),
-            Expanded(
-              child: ListenableBuilder(
-                listenable: _controller,
-                builder: (context, child) {
-                  if (_controller.tasks.isEmpty) {
-                    return Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          'Nenhuma tarefa adicionada, deseja adicionar?',
-                          style: TextStyle(
-                            fontSize: 16,
-                            color: Colors.grey,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 16.0,
+            vertical: 24.0,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Bem vindo!'),
+              SizedBox(height: 12),
+              Text('Pronto para finalizar as tarefas?'),
+              SizedBox(height: 24),
+              Expanded(
+                child: ListenableBuilder(
+                  listenable: _controller,
+                  builder: (context, child) {
+                    if (_controller.tasks.isEmpty) {
+                      return Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Nenhuma tarefa adicionada, deseja adicionar?',
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.grey,
+                            ),
                           ),
-                        ),
-                        SizedBox(height: 8),
-                        TextButton(
-                          onPressed: () => context.push('/add'),
-                          child: Text('Clique aqui para adicionar'),
-                        ),
-                      ],
-                    );
-                  }
-
-                  // Se houver tarefas, exibe a lista
-                  return ListView.builder(
-                    itemCount: _controller.tasks.length,
-                    itemBuilder: (context, index) {
-                      final task = _controller.tasks[index];
-                      return ListTile(
-                        title: Text(task.title),
-                        trailing: IconButton(
-                          icon: Icon(Icons.delete),
-                          onPressed: () => _controller.removeTask(task),
-                        ),
+                          SizedBox(height: 8),
+                          TextButton(
+                            onPressed: () => context.push('/add'),
+                            child: Text('Clique aqui para adicionar'),
+                          ),
+                        ],
                       );
-                    },
-                  );
-                },
+                    }
+
+                    // Se houver tarefas, exibe a lista
+                    return ListView.builder(
+                      itemCount: _controller.tasks.length,
+                      itemBuilder: (context, index) {
+                        final task = _controller.tasks[index];
+                        return ListTile(
+                          title: Text(task.title),
+                          trailing: IconButton(
+                            icon: Icon(Icons.delete),
+                            onPressed: () => _controller.removeTask(task),
+                          ),
+                        );
+                      },
+                    );
+                  },
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
